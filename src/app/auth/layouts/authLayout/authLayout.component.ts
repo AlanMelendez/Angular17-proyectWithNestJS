c6f4665616b2bf092db605ee24b761e449b1b997
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2024-05-17 22:02:04 -0600

    Add routes from proyect and lazy loading with standalone components.

--- src/app/auth/layouts/authLayout/authLayout.component.ts
+++ src/app/auth/layouts/authLayout/authLayout.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
-  template: `<p>authLayout works!</p>`,
+  template: `
+  <h2>Hi from layout component!</h2>
+
+    <router-outlet></router-outlet>
+
+  `,
   styleUrl: './authLayout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
